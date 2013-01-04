f0ec2561a90ae4e03e7dd53fa66575ddf9736439
ChunmengLu <qq596392912@gmail.com>
2013-01-04 17:28:08 +0800

    20120104

--- snode.sql
+++ snode.sql
@@ -118,30 +118,15 @@ CREATE TABLE `tag` (
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nick_name` varchar(50) DEFAULT NULL,
   `name` varchar(50) DEFAULT NULL,
   `pwd` varchar(64) DEFAULT NULL,
   `sex` int(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
-
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', '卢春梦', '123456', '1');
-INSERT INTO `user` VALUES ('2', '卢春梦', '123456', '1');
-INSERT INTO `user` VALUES ('3', '卢春梦', '123456', '1');
-INSERT INTO `user` VALUES ('4', '卢春梦', '123456', '1');
-INSERT INTO `user` VALUES ('5', '卢春梦', '123456', '1');
-INSERT INTO `user` VALUES ('6', '卢春梦', '123456', '1');
-INSERT INTO `user` VALUES ('7', '卢春梦', '123456', '1');
-INSERT INTO `user` VALUES ('8', '卢春梦', '123456', '1');
-INSERT INTO `user` VALUES ('9', '卢春梦', '123456', '1');
-INSERT INTO `user` VALUES ('10', '卢春梦', '123456', '1');
-INSERT INTO `user` VALUES ('11', '卢春梦', '123456', '1');
-INSERT INTO `user` VALUES ('12', '卢春梦', '123456', '1');
-INSERT INTO `user` VALUES ('13', '卢春梦', '123456', '1');
-INSERT INTO `user` VALUES ('14', '卢春梦', '123456', '1');
-INSERT INTO `user` VALUES ('15', '卢春梦', '123456', '1');
 
 -- ----------------------------
 -- Table structure for `user_info`
