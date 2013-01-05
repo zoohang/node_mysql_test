72f8aa64fcbcd54ab66b5a347963f77a5e93d71b
ChunMengLu <qq596392912@gmail.com>
2013-01-05 11:03:55 +0800

    20130105

--- controllers/index.js
+++ controllers/index.js
@@ -3,19 +3,19 @@
  */
 var encrypt = require("../util/encryptUtil")
   , db = require('../dao/db')
-  , config = require('../config');
+  , config = require('../config')
+  , dateUtil = require('../util/dateUtil');
 
 exports.auth = function(req, res, next){
     if(req.session.user){
         res.locals.user = req.session.user;
         console.log("has session!");
-        return next();
     }else{
         var cookie = req.cookies.snode_user;
         if (cookie){
             var userName = encrypt.aesDecrypt(cookie, config.secret);
             console.log(userName);
-            var sql = "SELECT id,sex FROM user where name=?";
+            var sql = "SELECT id,sex FROM user where email=?";
             db.query(sql,[userName], function(error, json) {
                 if(error) {
                     console.log(error);
@@ -31,17 +31,17 @@ exports.auth = function(req, res, next){
                 console.log("has cookie!");
                 /**
                  * 说明，由于nodejs的异步 在mysql查询之前会先返回页面
-                 * 所以将 next() 分开来写
+                 * 所以将 next() 得分开来写
                  */
                 return next();
             });
-        }else{
-            console.log("not has cookie!");
-            return next();
         }
     }
+    return next();
 }
 
 exports.index = function(req, res){
+    var df = dateUtil.dataFormat();
+    console.log("time:\t" + df);
     res.render('index', { title: 'snode', index: 'activd'});
 };
---
+++ util/dateUtil.js
@@ -0,0 +1,41 @@
+/**
+ * 时间对象的格式化;
+ * 扩展 format 方法
+ */
+Date.prototype.format = function(format) {
+    /*
+     * eg:format="yyyy-MM-dd hh:mm:ss";
+     */
+    var o = {
+        "M+" : this.getMonth() + 1, // month
+        "d+" : this.getDate(), // day
+        "h+" : this.getHours(), // hour
+        "m+" : this.getMinutes(), // minute
+        "s+" : this.getSeconds(), // second
+        "q+" : Math.floor((this.getMonth() + 3) / 3), // quarter
+        "S" : this.getMilliseconds()  // millisecond
+    }
+
+    if (/(y+)/.test(format)) {
+        format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4
+            - RegExp.$1.length));
+    }
+
+    for (var k in o) {
+        if (new RegExp("(" + k + ")").test(format)) {
+            format = format.replace(RegExp.$1, RegExp.$1.length == 1
+                ? o[k]
+                : ("00" + o[k]).substr(("" + o[k]).length));
+        }
+    }
+    return format;
+}
+
+/**
+ * 时间格式化
+ * yyyy-MM-dd hh:mm:ss
+ * @return {*}
+ */
+exports.dataFormat = function(){
+    return new Date().format("yyyy-MM-dd hh:mm:ss")
+}
